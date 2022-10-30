1635948c017491e5e29850f46f4938c1138f51ba
Luz <99538276+Luz-GM@users.noreply.github.com>
2022-10-30 00:52:14 -0300

    updated allowed cors from localhost to firebase

--- src/main/java/com/portfolio/mlgm/controller/CEducacion.java
+++ src/main/java/com/portfolio/mlgm/controller/CEducacion.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https://portfolio-f9423.web.app/")
+@CrossOrigin(origins = "https://portfolio-f9423.web.app")
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
